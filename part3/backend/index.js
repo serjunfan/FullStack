97bb6e44317fe0f03c54ace52f4ab0413a62eb21
serjunfan <107703042@nccu.edu.tw>
2023-03-12 00:18:10 +0800

    part3.c

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+const Person = require('./models/person') 
+
 const app = express()
 
 app.use(cors())
@@ -40,7 +43,9 @@ app.get('/info', (request, response) =>{
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(people => {
+    response.json(people)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -96,7 +101,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
